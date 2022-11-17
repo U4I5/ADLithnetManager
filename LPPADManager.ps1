ce07465cdd18ca85d7fc297f4dd3ea6ad38689b1
Yurisu13 <ulysse.oberlin13@gmail.com>
2022-11-17 11:45:48 +0100

    Correction for the Options script call

--- LPPADManager.ps1
+++ LPPADManager.ps1
@@ -14,32 +14,33 @@ Import-Module LithnetPasswordProtection
 function Show-Menu {
     param (
         [string]$Title = @"
-    -------------------------------------------------------------------------
+
+    -------------------------------------------------------------
      __    ____  ____  _____ ____  _____
     |  |  |  _  \  _  \  _  |    \|     |___ ___ ___ ___ ___ ___
-    |  |__|   __/   __/     |  |  | | | | .'|   | .'| . | -_|  _|
+    |  |__|   __/   __/     |  |  | | | | .'|   | .'| . | -_|  _/
     |_____|__|  |__|  |__|__|____/|_|_|_|__,|_|_|__,|_  |___|_|
                                                     |___|
-    -------------------------------------------------------------------------
+    -------------------------------------------------------------
 
 
 "@
     )
     Clear-Host
     Write-Host $Title  -ForegroundColor DarkCyan
-    Write-Host "[+]: Type '1' To Add A Banned Word." -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '2' To Test A banned Word."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '3' To Test A Compromised Password."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '4' To Remove A Banned Word." -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '5' To Remove A Compromised Password."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '6' To Remove A List Of Banned Words."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '7' To Remove A List Of Compromised Passwords."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '8' To Import A List Of Banned Words."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '9' To Import A List Of Compromised Passwords."  -ForegroundColor DarkGreen
-    Write-Host "[+]: Type '10' To Import A List Of Compromised Password NTLM Hashes."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '1' To Add A Banned Word." -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '2' To Test A banned Word."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '3' To Test A Compromised Password."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '4' To Remove A Banned Word." -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '5' To Remove A Compromised Password."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '6' To Remove A List Of Banned Words."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '7' To Remove A List Of Compromised Passwords."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '8' To Import A List Of Banned Words."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '9' To Import A List Of Compromised Passwords."  -ForegroundColor DarkGreen
+    Write-Host "  [+]: Type '10' To Import A List Of Compromised Password NTLM Hashes."  -ForegroundColor DarkGreen
     Write-Host
-    Write-Host "[+]: Type 'L' To Print Logs Of Banned Words Added" -ForegroundColor DarkYellow
-    Write-Host "[+]: Type 'Q' To Quit." -ForegroundColor Red
+    Write-Host "  [+]: Type 'L' To Print Logs Of Banned Words Added" -ForegroundColor DarkYellow
+    Write-Host "  [+]: Type 'Q' To Quit." -ForegroundColor Red
     Write-Host
 
 }
@@ -47,7 +48,7 @@ function Show-Menu {
 do {
     #Prints the menu and ask an option to display
     Show-Menu
-    $selection = Read-Host "Select An Option"
+    $selection = Read-Host "  Select An Option"
 
     #Checks if "Q" is selected to exit or continue to the option selection
     if ($selection -eq 'q') {exit}
@@ -55,7 +56,7 @@ do {
         #Sets the content of the $selection variable in a temporary text file
         Set-Content .\selection.txt $selection
         #Opens the selected option in a new powershell window
-        Start-Process powershell {powershell.exe -file '.\LithnetManagerOptions.ps1'}
+        Start-Process powershell {powershell.exe -file '.\LPPADManagerOptions.ps1'}
     }
 }
 until ('a' -ne 'a')
